49f4687e5e3adfcc5f234ad0f28859db9141be87
MPcooray <it23194830@my.sliit.lk>
2025-03-17 14:38:13 +0530

    ReadME updated & backend

---
+++ mandala-backend/src/test/java/com/mandala/MandalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
